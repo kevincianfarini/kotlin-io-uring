06bade0d2592fb1fd5877338ec8c579ad62bac9f
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-12-27 18:58:46 -0500

    Implement vectorRead

--- src/linuxX64Test/kotlin/com/kevincianfarini/iouring/URingTest.kt
+++ src/linuxX64Test/kotlin/com/kevincianfarini/iouring/URingTest.kt
@@ -2,9 +2,7 @@ package com.kevincianfarini.iouring
 
 import kotlinx.coroutines.runBlocking
 import liburing.*
-import kotlin.test.Test
-import kotlin.test.assertFailsWith
-import kotlin.test.assertTrue
+import kotlin.test.*
 
 class URingTest {
 
@@ -30,6 +28,19 @@ class URingTest {
     @Test fun `URing fails to close bad file descriptor`() = runBlocking {
         URing(QueueDepth(2u), 0u).use { ring ->
             assertFailsWith<IllegalStateException> { ring.close(-1) }
+        }
+    }
+
+    @Test fun `URing vectorRead reads into buffer`() = runBlocking {
+        URing(QueueDepth(2u), 0u).use { ring ->
+            val fd = ring.open("./src/linuxX64Test/resources/hello.txt")
+            val buffer = ByteArray(100)
+            val bytesRead = ring.vectorRead(fd, buffer)
+            assertTrue(bytesRead > 0)
+            assertEquals(
+                expected = "hello world!",
+                actual = buffer.decodeToString(endIndex = bytesRead),
+            )
         }
     }
 }
